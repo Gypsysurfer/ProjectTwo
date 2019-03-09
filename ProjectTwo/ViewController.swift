fa065ba51d742c2293edf87188a34d7ff8d79596
Sergio San Pedro <sergio.sanpedro@me.com>
2019-03-08 20:25:25 -1000

    Initial Commit

---
+++ ProjectTwo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProjectTwo
+//
+//  Created by Sergio San Pedro on 3/8/19.
+//  Copyright © 2019 Sergio San Pedro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
