29158d32737f909e6b6a58f2f2950aa5631388bc
Sergio San Pedro <sergio.sanpedro@me.com>
2019-03-14 09:49:04 -1000

    labels

--- ProjectTwo/ViewController.swift
+++ ProjectTwo/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  ProjectTwo
 //
-//  Created by Sergio San Pedro on 3/8/19.
+//  Created by Sergio San Pedro on 3/14/19.
 //  Copyright © 2019 Sergio San Pedro. All rights reserved.
 //
 
@@ -10,13 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var firstLabel: UILabel!
+    
+    @IBOutlet var secondLabel: UILabel!
+    
+    @IBOutlet var thirdLabel: UILabel!
+    
+    @IBOutlet var fourthLabel: UILabel!
+    
+    @IBOutlet var fifthLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
-    
-    }
 
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
-
